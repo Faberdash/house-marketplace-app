337681c28417d323e0ed3907df808779034a46b9
Mo'men Alaswad <faberdash55@gmail.com>
2022-01-29 21:42:03 +0200

    Added Image Carousel To Listings And Explore Pages, Added User Listings To Profile Page With Delete Functionality.

---
+++ src/components/Slider.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { collection, getDocs, query, orderBy, limit } from 'firebase/firestore'
+import { db } from '../firebase.config'
+import SwiperCore, { Navigation, Pagination, Scrollbar, A11y } from 'swiper'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/swiper-bundle.css'
+import Spinner from './Spinner'
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y])
+
+function Slider() {
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const getListings = async () => {
+      const listingsRef = collection(db, 'listings')
+      const q = query(listingsRef, orderBy('timestamp', 'desc'), limit(5))
+      const querySnap = await getDocs(q)
+
+      let listingsArr = []
+
+      querySnap.forEach((doc) => {
+        return listingsArr.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setListings(listingsArr)
+      setLoading(false)
+    }
+
+    getListings()
+  }, [])
+
+  if (loading) {
+    return <Spinner />
+  }
+
+  return (
+    listings && (
+      <>
+        <p className='exploreHeading'>Latest Listings</p>
+
+        <Swiper slidesPerView={1} pagination={{ clickable: true }}>
+          {listings.map(({ data, id }) => (
+            <SwiperSlide
+              key={id}
+              onClick={() => navigate(`/category/${data.type}/${id}`)}
+            >
+              <div
+                className='swiperSlideDiv'
+                style={{
+                  background: `url(${data.imgUrls[0]}) center no-repeat`,
+                  backgroundSize: 'cover',
+                }}
+              >
+                <p className='swiperSlideText'>{data.name}</p>
+                <p className='swiperSlidePrice'>
+                  ${data.discountedPrice ?? data.regularPrice}{' '}
+                  {data.type === 'rent' && '/ Month'}
+                </p>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </>
+    )
+  )
+}
+
+export default Slider
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,19 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { getAuth, updateProfile } from 'firebase/auth'
 import { db } from '../firebase.config'
-import { doc, updateDoc } from 'firebase/firestore'
+import {
+  doc,
+  updateDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  orderBy,
+  deleteDoc,
+} from 'firebase/firestore'
 import { toast } from 'react-toastify'
+import ListingItem from '../components/ListingItem'
 import arrowRight from '../assets/svg/keyboardArrowRightIcon.svg'
 import homeIcon from '../assets/svg/homeIcon.svg'
 
@@ -16,6 +26,9 @@ function Profile() {
   const { name, email } = formData
 
   const [changeDetails, setChangeDetails] = useState(false)
+
+  const [listings, setListings] = useState(null)
+  const [loading, setLoading] = useState(true)
 
   const navigate = useNavigate()
 
@@ -51,6 +64,50 @@ function Profile() {
       toast.error('Could not update profile details')
     }
   }
+
+  // Delete a Listing Item
+  const onDelete = async (listingId) => {
+    if (window.confirm('Are you sure you want to delete this listing?')) {
+      try {
+        await deleteDoc(doc(db, 'listings', listingId))
+
+        const updatedListings = listings.filter(
+          (listing) => listing.id !== listingId
+        )
+        setListings(updatedListings)
+        toast.success('Listing deleted sucessfully')
+      } catch (error) {
+        toast.error('Could not delete listing at the moment')
+      }
+    }
+  }
+
+  useEffect(() => {
+    const fetchUsersListings = async () => {
+      const listingsRef = collection(db, 'listings')
+      const q = query(
+        listingsRef,
+        where('userRef', '==', auth.currentUser.uid),
+        orderBy('timestamp', 'desc')
+      )
+
+      const querySnap = await getDocs(q)
+
+      const listingsArr = []
+
+      querySnap.forEach((doc) => {
+        return listingsArr.push({
+          id: doc.id,
+          data: doc.data(),
+        })
+      })
+
+      setListings(listingsArr)
+      setLoading(false)
+    }
+
+    fetchUsersListings()
+  }, [auth.currentUser.uid])
 
   return (
     <div className='profile'>
@@ -102,6 +159,22 @@ function Profile() {
           <p>Sell or rent your home</p>
           <img src={arrowRight} alt='Arrow Right' />
         </Link>
+
+        {!loading && listings?.length > 0 && (
+          <>
+            <p className='listingText'>Your Listings</p>
+            <ul className='listingsList'>
+              {listings.map((listing) => (
+                <ListingItem
+                  key={listing.id}
+                  listing={listing.data}
+                  id={listing.id}
+                  onDelete={() => onDelete(listing.id)}
+                />
+              ))}
+            </ul>
+          </>
+        )}
       </main>
     </div>
   )
